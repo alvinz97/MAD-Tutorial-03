7d77f38891e2b26741be47113ca6efb2cd874844
[alvinz97] <it18211306@my.sliit.lk>
2020-03-20 13:41:42 +0530

    Android Fragments Project Tutorial 3

---
+++ app/src/main/java/tute02/sliit/androidfragments/MainActivity.java
@@ -0,0 +1,14 @@
+package tute02.sliit.androidfragments;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
